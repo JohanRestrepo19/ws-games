f01bc36f87030f6f56bf1c1d9f2c5f6657193dea
Johan Restrepo <62318029+JohanRestrepo19@users.noreply.github.com>
2024-03-01 21:36:31 -0500

    feat: TicTacToe class partially implemented.

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,27 @@
-import {myGreetingFunc} from '@/models/TicTacToe';
+import TicTacToe, {type GameState} from '@/models/TicTacToe';
 
 function main() {
-    myGreetingFunc('Johan');
+    const game = new TicTacToe('O');
+    let gameResult: GameState;
+
+    gameResult = game.makeMove({row: 0, col: 0});
+    console.log('Winner from main: ', gameResult);
+
+    gameResult = game.makeMove({row: 1, col: 0});
+    console.log('Winner from main: ', gameResult);
+
+    gameResult = game.makeMove({row: 0, col: 1});
+    console.log('Winner from main: ', gameResult);
+
+    gameResult = game.makeMove({row: 1, col: 1});
+    console.log('Winner from main: ', gameResult);
+
+    gameResult = game.makeMove({row: 0, col: 2});
+    console.log('Winner from main: ', gameResult);
+
+    gameResult = game.makeMove({row: 1, col: 2});
+    console.log('Winner from main: ', gameResult);
 }
 
+//TODO: Test my TicTacToe class
 main();
--- src/models/TicTacToe.ts
+++ src/models/TicTacToe.ts
@@ -1,3 +1,90 @@
-export const myGreetingFunc = (name: string) => {
-    console.log('hello: ', name, 'from my class file');
-};
+export type Piece = 'X' | 'O';
+export type Cell = Piece | null;
+export type Position = {row: number; col: number};
+export type GameState = Piece | 'playing' | 'draw';
+
+// prettier-ignore
+const WINNER_SET_POSITIONS: Position[][] = [
+    //Rows
+    [ {row: 0, col: 0}, {row: 0, col: 1}, {row: 0, col: 2} ],
+    [ {row: 1, col: 0}, {row: 1, col: 1}, {row: 1, col: 2} ],
+    [ {row: 2, col: 0}, {row: 2, col: 1}, {row: 2, col: 2} ],
+    //Cols
+    [ {row: 0, col: 0}, {row: 1, col: 0}, {row: 2, col: 0} ],
+    [ {row: 0, col: 1}, {row: 1, col: 1}, {row: 2, col: 1} ],
+    [ {row: 0, col: 2}, {row: 1, col: 2}, {row: 2, col: 2} ],
+    //Diagonals
+    [ {row: 0, col: 0}, {row: 1, col: 1}, {row: 2, col: 2} ],
+    [ {row: 0, col: 2}, {row: 1, col: 1}, {row: 2, col: 0} ],
+];
+
+export default class TicTacToe {
+    currentPiece: Piece;
+    private gameState: GameState;
+    private availableMoves: number;
+    private board: Cell[][];
+
+    constructor(initialPiece: Piece = 'X') {
+        this.currentPiece = initialPiece;
+        this.board = [];
+        this.gameState = 'playing';
+        this.availableMoves = 9;
+
+        for (let rows = 0; rows < 3; rows++) {
+            const row = [null, null, null];
+            this.board.push(row);
+        }
+    }
+
+    makeMove(position: Position): GameState {
+        const {row, col} = position;
+
+        if (
+            this.gameState === 'draw' ||
+            this.gameState === 'O' ||
+            this.gameState === 'X'
+        ) {
+            return this.gameState;
+        }
+
+        // Check for valid move
+        if (this.board[row][col]) throw new Error('Not a valid move!');
+
+        this.board[row][col] = this.currentPiece;
+        this.availableMoves--;
+
+        this.checkGameState();
+        this.printBoard();
+
+        this.currentPiece = this.currentPiece === 'X' ? 'O' : 'X';
+        return this.gameState;
+    }
+
+    checkGameState(): void {
+        // I have to check inside my board if one of two players has won
+        const players = ['X', 'O'] as const;
+
+        for (const player of players) {
+            // Check rows, cols and diagonals
+            for (const set of WINNER_SET_POSITIONS) {
+                const content = set.map<Cell>(
+                    ({row, col}) => this.board[row][col],
+                );
+                const isWinner = content.every(cell => cell === player);
+                if (isWinner) {
+                    this.gameState = player;
+                    break;
+                }
+            }
+        }
+
+        // At this point I need to check if is in a Draw State
+        if (this.availableMoves === 0) this.gameState = 'draw';
+    }
+
+    printBoard(): void {
+        console.log('my game: ');
+        this.board.forEach(row => console.log(row));
+        console.log();
+    }
+}
