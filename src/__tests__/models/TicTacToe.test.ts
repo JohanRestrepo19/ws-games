aaae992bd3542c603e13960696fb5ce60108f2ad
Johan Restrepo <62318029+JohanRestrepo19@users.noreply.github.com>
2024-03-04 23:16:11 -0500

    test: add TicTacToe class respective tests.

--- src/__tests__/models/TicTacToe.test.ts
+++ src/__tests__/models/TicTacToe.test.ts
@@ -1,10 +1,82 @@
 import TicTacToe, {type Piece} from '@/models/TicTacToe';
 
 describe('Test TicTacToe Class', () => {
-    test('Mark correct place', () => {
-        const game = new TicTacToe('O');
-        const [row, col] = [0, 0];
-        game.makeMove({row, col});
-        expect(game.currentBoard[row][col]).toBe<Piece>('O');
+    let game: TicTacToe;
+
+    beforeEach(() => {
+        game = new TicTacToe();
+    });
+
+    describe('Initialization', () => {
+        test('Should initialize with X', () => {
+            expect(game.currentPiece).toBe<Piece>('X');
+        });
+
+        test('Should initialize with O', () => {
+            game = new TicTacToe('O');
+            expect(game.currentPiece).toBe<Piece>('O');
+        });
+
+        test('should initialize with available moves set to 9', () => {
+            expect(game.currentAvailableMoves).toBe(9);
+        });
+
+        test('should initialize with an empty board', () => {
+            expect(game.currentBoard).toEqual([
+                [null, null, null],
+                [null, null, null],
+                [null, null, null],
+            ]);
+        });
+    });
+
+    describe('Movements', () => {
+        test('should make a valid move and switch player', () => {
+            const initialGameState = game.makeMove({row: 0, col: 0});
+            expect(initialGameState).toBe('playing');
+            expect(game.currentPiece).toBe('O');
+            expect(game.currentBoard[0][0]).toBe('X');
+        });
+
+        test('should not allow making an invalid move', () => {
+            game.makeMove({row: 0, col: 0});
+            expect(() => game.makeMove({row: 0, col: 0})).toThrow(
+                'Not a valid move!',
+            );
+        });
+
+        test('should detect a win for X', () => {
+            game.makeMove({row: 0, col: 0});
+            game.makeMove({row: 1, col: 0});
+            game.makeMove({row: 0, col: 1});
+            game.makeMove({row: 1, col: 1});
+            const gameState = game.makeMove({row: 0, col: 2});
+            expect(gameState).toBe('X');
+        });
+
+        test('should detect a win for O', () => {
+            game = new TicTacToe('O');
+            game.makeMove({row: 0, col: 0});
+            game.makeMove({row: 1, col: 0});
+            game.makeMove({row: 0, col: 1});
+            game.makeMove({row: 1, col: 1});
+            const gameState = game.makeMove({row: 0, col: 2});
+            expect(gameState).toBe('O');
+        });
+
+        test('should detect a draw when no moves are left', () => {
+            game = new TicTacToe('O');
+            game.makeMove({row: 0, col: 0});
+            game.makeMove({row: 1, col: 1});
+            game.makeMove({row: 1, col: 0});
+            game.makeMove({row: 2, col: 0});
+            game.makeMove({row: 0, col: 2});
+            game.makeMove({row: 0, col: 1});
+            game.makeMove({row: 2, col: 1});
+            game.makeMove({row: 1, col: 2});
+
+            const gameState = game.makeMove({row: 2, col: 2});
+            expect(gameState).toBe('draw');
+        });
     });
 });
