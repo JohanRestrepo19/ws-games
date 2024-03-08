c3684394b23f221086de092a239347213c72f5bb
Johan Restrepo <62318029+JohanRestrepo19@users.noreply.github.com>
2024-03-08 16:59:15 -0500

    feat: intial configuration for socket io.

--- README.md
+++ README.md
@@ -1,7 +1,12 @@
 ## TODO:
 
 - [x] Define which Web Sockets library to use (Socket.io || WS) -> I'm going with Socket.io
+- [ ] Configuring cors (Install express middleware).
 - [ ] Activity Diagram. 
 - [ ] Class Diagram.
 - [ ] Define which technology to use on the client (Next.js || React + vite).
 - [ ] Define server folder structure.
+
+
+## Notes
+- I need to first declare logic of Rooms creation before even start playing.
--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,18 @@
 import express from 'express';
 import {createServer} from 'node:http';
+import {Server} from 'socket.io';
 
-const app = express();
-const server = createServer(app);
+const requestListener = express();
+const server = createServer(requestListener);
+const io = new Server(server);
 const port = process.env.PORT || 8080;
 
-app.get('/', (_, res) => {
-    res.send('<h1>Hellow World!</h1>');
+requestListener.get('/', (_, res) => {
+    res.send('<h1>Not allowed!</h1>');
+});
+
+io.on('connection', socket => {
+    console.log('A user has connected with this info: ', socket);
 });
 
 server.listen(port, () => {
